b93192b3c7a8d01da7e007ca6b0ef39add70e28e
Daniel <ddoherty6@gmail.com>
2022-02-20 18:17:54 -0800

    trying to fix delete button

--- public/javascript/delete-post.js
+++ public/javascript/delete-post.js
@@ -21,4 +21,4 @@ async function deleteFormHandler(event) {
   
   }
   
-  document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
+  document.getElementById('#delete-post-btn').addEventListener('click', deleteFormHandler);
